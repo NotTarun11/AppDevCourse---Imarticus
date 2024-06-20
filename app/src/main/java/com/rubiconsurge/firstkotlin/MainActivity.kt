8e73795c5289eabcd2e235ca5a9d4840e54c925e
Tarun Parthiban <tarunpr1103@gmail.com>
2024-06-20 14:29:37 +0530

    Assignment-3

--- app/src/main/java/com/rubiconsurge/firstkotlin/MainActivity.kt
+++ app/src/main/java/com/rubiconsurge/firstkotlin/MainActivity.kt
@@ -3,11 +3,28 @@ package com.rubiconsurge.firstkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import java.util.Locale
 
+private var myLocale: Locale? = null
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val greet = findViewById<TextView>(R.id.textView3)
     }
+
+
+
+    fun switchTamil(view: View) {
+        val myLocale = Locale("ta")
+        saveLo
+        val greet = findViewById<TextView>(R.id.textView3)
+        greet.text = getString(R.string.greetingTamil)
+
+
+    }
+    fun switchEnglish(view: View) {}
 
 }
