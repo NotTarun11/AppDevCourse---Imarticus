7d6ff27ce1e4730c0b30296a98453f75caa2601e
Tarun Parthiban <tarunpr1103@gmail.com>
2024-06-20 15:04:19 +0530

    Assignment-4

--- app/src/main/java/com/rubiconsurge/firstkotlin/MainActivity.kt
+++ app/src/main/java/com/rubiconsurge/firstkotlin/MainActivity.kt
@@ -1,30 +1,47 @@
 package com.rubiconsurge.firstkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import java.util.Locale
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-private var myLocale: Locale? = null
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val greet = findViewById<TextView>(R.id.textView3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val inputField = findViewById<EditText>(R.id.Name)
+        val password = findViewById<EditText>(R.id.password)
+        val submitButton = findViewById<Button>(R.id.button1)
+        var enteredName = ""
+        var pass = ""
+        submitButton.setOnClickListener {
+            enteredName = inputField.text.toString()
+            pass = password.text.toString()
+            if (enteredName == ""){
+                Toast.makeText(this@MainActivity,"Please, Enter your name",Toast.LENGTH_SHORT).show()
+            }
+            else {
+                if (pass == "1234") {
+                    val intent = Intent(this, SecondScreen::class.java)
+                    intent.putExtra("USER",enteredName)
+                    startActivity(intent)
+                }
+                else {
+                    Toast.makeText(this@MainActivity,"Incorrect Password",Toast.LENGTH_SHORT).show()
+                    password.text.clear()
+                }
+            }
+        }
+
+
     }
-
-
-
-    fun switchTamil(view: View) {
-        val myLocale = Locale("ta")
-        saveLo
-        val greet = findViewById<TextView>(R.id.textView3)
-        greet.text = getString(R.string.greetingTamil)
-
-
-    }
-    fun switchEnglish(view: View) {}
-
 }
