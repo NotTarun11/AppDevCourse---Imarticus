99a0810dc2b748ab095799003cefb2930420eece
Tarun Parthiban <tarunpr1103@gmail.com>
2024-06-21 13:12:28 +0530

    Button click log

--- app/src/main/java/com/rubiconsurge/firstkotlin/MainActivity.kt
+++ app/src/main/java/com/rubiconsurge/firstkotlin/MainActivity.kt
@@ -2,22 +2,29 @@ package com.rubiconsurge.firstkotlin
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.rubiconsurge.firstkotlin.R.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+    }
+    fun clickHandler(view: View) {
+        Log.i("MainActivity-clickhandler","button clicked")
     }
 
 }
