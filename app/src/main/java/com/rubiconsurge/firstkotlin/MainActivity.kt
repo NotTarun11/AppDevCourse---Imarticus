7d8c9dc4c3fa50ad9c8a0645411a52bf1835507b
Tarun Parthiban <tarunpr1103@gmail.com>
2024-06-21 11:44:33 +0530

    Initial commit

---
+++ app/src/main/java/com/rubiconsurge/firstkotlin/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.rubiconsurge.firstkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
