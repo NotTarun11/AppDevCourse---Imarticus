b2309886ca58b7e03d68add9aab2b276eae5d3fc
Tarun Parthiban <tarunpr1103@gmail.com>
2024-06-21 12:20:23 +0530

    Kotlin example

---
+++ app/src/main/java/com/examples/Demo.kt
@@ -0,0 +1,12 @@
+package com.examples
+
+fun main() {
+
+    val numberOfFish = 50
+    val numberOfPlants = 23
+    if (numberOfFish > numberOfPlants) {
+        println("Good ratio!")
+    } else {
+        println("Unhealthy ratio")
+    }
+}
